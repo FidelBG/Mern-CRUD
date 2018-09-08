1276b001e3621b4e1db4fd135ab208032f9f0917
unknown <ghisote@gmail.com>
2018-09-09 01:47:42 +0200

    pass

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://dbuser:xxxxxxx@ds125362.mlab.com:25362/react-express';
+const URI = 'mongodb://dbuser:dbuser1@ds125362.mlab.com:25362/react-express';
 
 mongoose.connect(URI)
   .then(db => console.log('Db is connected'))
